329571c2745996c11d99d40e6bfea9be23c6903f
root <yogeeshwarm@brightcomgroup.com>
2023-01-02 01:52:30 -0800

    Task-9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 if number < 0:
     digit = -digit
@@ -10,4 +8,4 @@ if digit > 5:
 elif digit == 0:
     print("0")
 else:
-    print("less than 6 and not 0")
+    print("less than 6 and not ")
