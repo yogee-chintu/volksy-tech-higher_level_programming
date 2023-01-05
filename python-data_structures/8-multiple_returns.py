3488775b09ff6a7ccd47d560c849e17bf1e37048
root <yogeeshwarm@brightcomgroup.com>
2023-01-05 03:54:19 -0800

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,4 @@
 def multiple_returns(sentence):
     if(len(sentence) > 0):
         return((len(sentence), sentence[0]))
-    return((len(sentence), None))#!/usr/bin/python3
-def multiple_returns(sentence):
-    if(len(sentence) > 0):
-        return((len(sentence), sentence[0]))
     return((len(sentence), None))
