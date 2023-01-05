d81ef7bad4ce252b31b9a69bbe946263d67408db
root <yogeeshwarm@brightcomgroup.com>
2023-01-05 03:59:26 -0800

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     a = tuple_a or (0, 0)
     b = tuple_b or (0, 0)
-    if(len(a) is 1):
+    if(len(a) == 1):
         a = (tuple_a[0], 0)
     if(len(b) is 1):
         b = (tuple_b[0], 0)
