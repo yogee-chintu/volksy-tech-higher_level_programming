cc2241097e7a3f476bf2223d407950734e899547
root <yogeeshwarm@brightcomgroup.com>
2023-01-11 02:53:20 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise TypeError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
 
 
