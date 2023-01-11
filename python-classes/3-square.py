d531416b250c019e5c5cdad51faf38925d1d9ac0
root <yogeeshwarm@brightcomgroup.com>
2023-01-11 04:17:16 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python3
-"""THIS CODE WRITTEN BY YOGEE"""
+"""
+Module 3-square
+Defines class Square with private attribute size and public attribute area
+"""
 
 
 class Square:
-    """This is asquare class"""
-    def __init__(self, size=0)
-    if type(size) != int:
-        raise TypeError("size must be intezer")
-    if size < 0:
-        raise ValueError("size must be >=0")
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size)
+        area(self)
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes square
+        Attributes:
+            __size (int): size of a side of square, defaults to 0 if none
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
     def area(self):
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
