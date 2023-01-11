8a8d016e83aa80a19ceb98aa45505b2a6e4eb576
root <yogeeshwarm@brightcomgroup.com>
2023-01-11 02:50:43 -0800

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""THIS CODE WRIITEN BY YOGEE"""
+
+
+class Square:
+    """This is square class"""
+    def __init__(self, size=0):
+        """
+        The init method
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+
