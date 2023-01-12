e5d0d5b2d7e55c2b2f54ef983132a643a6e15c5b
root <yogeeshwarm@brightcomgroup.com>
2023-01-11 22:46:58 -0800

    100-singly_linked_list.py

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+Square Module
+"""
+
+
+class Node:
+    """
+    Node program
+    """
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) != int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if type(value) is not Node and value is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    SinglyLinkedList Module
+    """
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        tmp = self.__head
+        while tmp is not None:
+            string += str(tmp.data)
+            tmp = tmp.next_node
+            if tmp is not None:
+                string += "\n"
+        return string
+    def sorted_insert(self, value):
+        """
+        Inserts new nodes into singly linked list in sorted order
+        Args:
+        value: int data for node
+        """
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+            return
+
+        tmp = self.__head
+        if new.data < tmp.data:
+            new.next_node = self.__head
+            self.__head = new
+            return
+
+        while (tmp.next_node is not None) and (new.data > tmp.next_node.data):
+            tmp = tmp.next_node
+        new.next_node = tmp.next_node
+        tmp.next_node = new
+        return
+
