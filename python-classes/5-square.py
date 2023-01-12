c07dc44549e7f5f301fae5d0fc939d2e3776d1bc
root <yogeeshwarm@brightcomgroup.com>
2023-01-11 21:38:28 -0800

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -7,13 +7,15 @@ class Square:
     Square MOdule
     """
     def __init__(self, size=0):
-    """
-    Intialize square
-    """
+        """
+        Intialize square
+        """
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -22,12 +24,12 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
-        return (self.__size) ** 2
+        return (self.__size)**2
 
     def my_print(self):
         """
         Prints square with #'s
         """
         print("\n".join(["#" * self.__size for rows in range(self.__size)]))
-
