76fcfae248794445976ff8411c6b55482ddeab8d
root <yogeeshwarm@brightcomgroup.com>
2023-01-11 20:51:36 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,14 +8,17 @@ class Square:
     """
     def __init__(self, size=0):
         self.size = size
+
+    @property
     def size(self):
         return self.__size
+
+    @size.setter
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an intezer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
     def area(self):
         return (self.__size)**2
