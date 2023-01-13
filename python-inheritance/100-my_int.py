1f336af913bdb6de0d6a8e8664ec2f1e0f307062
root <yogeeshwarm@brightcomgroup.com>
2023-01-13 03:00:38 -0800

    100-my_int.py

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -10,3 +10,7 @@ class MyInt(int):
     def __eq__(self, value):
         """Override == opeartor with != behavior."""
         return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
