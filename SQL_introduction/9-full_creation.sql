c3e233493f0808c14a2dd6f3b9cb4365966a3d95
root <yogeeshwarm@brightcomgroup.com>
2023-01-29 23:07:43 -0800

    9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- creating second table in the database.
+CREATE TABLE second_table
+(id INT,name VARCHAR(256),score INT);
+INSERT INTO(id,name,score)
+VALUES(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
