e4780da0e3a864a9d0286d5e98937eb4c1454452
root <yogeeshwarm@brightcomgroup.com>
2023-01-31 03:44:56 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- creating and giving permissions.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
