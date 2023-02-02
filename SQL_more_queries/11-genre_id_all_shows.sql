84a9f6838356f5a245f96610ea138340cbbbeb65
root <yogeeshwarm@brightcomgroup.com>
2023-02-02 01:52:50 -0800

    11-genre_id_all_shows.sql

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- import database dump from hbtn-0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_shows_genres WHERE tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title,tv_show_genres.genre_id ASC;
