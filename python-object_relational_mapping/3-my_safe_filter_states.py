4a6be84db35bd7e4f9e9e2d8fe7879fb32f27574
root <yogeeshwarm@brightcomgroup.com>
2023-02-08 22:37:13 -0800

    3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0x0F. Python - ORM - task 2. Filter states by user input"""
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s "
+                "ORDER BY id ASC", (sys.argv[4], ))
+    s = cur.fetchall()
+    for y in s:
+        print(y)
+    cur.close()
+    conn.close()
