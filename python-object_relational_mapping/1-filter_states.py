263454e643d9d649037308e5ccaf2af3853c1990
root <yogeeshwarm@brightcomgroup.com>
2023-02-08 21:42:56 -0800

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""ors task-1"""
+
+if __name__ = 'main':
+    import sys
+    import MySQLdb
+    conn=MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+            "ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
