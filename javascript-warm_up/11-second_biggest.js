d1bebcd64b6f65f1784eac0f65f7c460c3f915d1
root <yogeeshwarm@brightcomgroup.com>
2023-02-21 22:15:34 -0800

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -10,3 +10,14 @@ if (process.argv.length < 4) {
     largest = parseInt(process.argv[3]);
     second = parseInt(process.argv[2]);
   }
+
+  for (let i = 4; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > largest) {
+      second = largest;
+      largest = parseInt(process.argv[i]);
+    } else if (parseInt(process.argv[i]) > second) {
+      second = parseInt(process.argv[i]);
+    }
+  }
+  console.log(second);
+}
